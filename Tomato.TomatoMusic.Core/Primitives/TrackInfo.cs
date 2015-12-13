27bbe60b10b2c61415d6089d2edc98358d94bc04
SunnyCase <sunnycase@live.cn>
2015-12-13 23:35:39 +0800

    添加Playlist模块

---
+++ Tomato.TomatoMusic.Core/Primitives/TrackInfo.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tomato.TomatoMusic.Primitives
+{
+    public class TrackInfo
+    {
+        public Uri Source { get; set; }
+        public string Title { get; set; }
+    }
+}
---
+++ Tomato.TomatoMusic.Playlist/Providers/PlaylistProvider.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.Storage;
+using Windows.Storage.Search;
+
+namespace Tomato.TomatoMusic.Playlist.Providers
+{
+    class PlaylistProvider
+    {
+        public PlaylistProvider()
+        {
+
+        }
+    }
+}
